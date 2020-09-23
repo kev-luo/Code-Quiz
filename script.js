ecafd51389e01f58d6f5c6865f0976f1d2833ead
Kevin Luo <kvn.luo@gmail.com>
2020-09-22 22:28:24 -0400

    add function for checking quiz responses

--- script.js
+++ script.js
@@ -16,9 +16,16 @@ var secondsLeft = document.querySelector("#timeLeft");
 var seconds = 100;
 var score = 0;
 
+// quiz question variables
+var answers = {"qu1": "this",
+"qu2": "that",
+"qu3": "or",
+"qu4": "perhaps",
+"qu5": "both"};
+
 // EVENT LISTENERS
 beginBtn.addEventListener("click",beginTimer);
-
+qu1.addEventListener("click",question);
 
 
 // FUNCTIONS
@@ -27,13 +34,33 @@ beginBtn.addEventListener("click",beginTimer);
 function beginTimer(event) {
     event.preventDefault();
     beginQuiz.setAttribute("style", "display: none");
+    qu1.setAttribute("style", "display: block");
 
     var timeInterval = setInterval(function() {
-        secondsLeft.textContent = seconds
         seconds--;
+        secondsLeft.textContent = seconds;
 
         if (seconds === 0) {
             clearInterval(timeInterval);
         }
     },1000);
+};
+
+// questions function
+function question(event) {
+    event.preventDefault();
+    var parent = event.target.parentElement.id;
+
+    if (event.target.matches("button")) {
+        if (event.target.textContent === answers[event.target.parentElement.id]) {
+            var ansCheck = document.createElement("p");
+            ansCheck.innerHTML = "<hr>"+ "Correct!";
+            event.target.parentElement.append(ansCheck);
+        }
+        else {
+            var ansCheck = document.createElement("p");
+            ansCheck.innerHTML = "<hr>"+ "Better luck next time!";
+            event.target.parentElement.append(ansCheck);
+        }
+    }
 }
